8160acf446599fe8dd1e161e7e1b93083a371c64
grecudd <grecudaniel123456789@gmail.com>
2022-08-31 15:47:11 +0300

    ImageExporter class

--- src/com/java/jpp/oscidrawing/io/ImageExporter.java
+++ src/com/java/jpp/oscidrawing/io/ImageExporter.java
@@ -1,9 +1,24 @@
 package com.java.jpp.oscidrawing.io;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class ImageExporter {
     public static boolean writeToPNG(String pathWithoutSuffix, BufferedImage img) {
-        throw new UnsupportedOperationException();
+        try {
+            String path = pathWithoutSuffix + ".pgn";
+            File outputImage = new File(path);
+
+            if(outputImage.exists()){
+                throw new Exception("Existent file");
+            }
+
+            ImageIO.write(img, ".pgn", outputImage);
+        } catch(Exception e){
+            return false;
+        }
+
+        return true;
     }
 }
