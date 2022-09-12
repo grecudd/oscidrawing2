bfcd1e00bea2148bc7b120fc9c83274cf04132a8
Daniel <grecudaniel123456789@gmail.com>
2022-09-12 17:06:25 +0300

    PathImporter fromFile

--- src/com/java/jpp/oscidrawing/io/PathImporter.java
+++ src/com/java/jpp/oscidrawing/io/PathImporter.java
@@ -2,6 +2,9 @@ package com.java.jpp.oscidrawing.io;
 
 import com.java.jpp.oscidrawing.generation.pathutils.Point;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,10 +27,26 @@ public class PathImporter {
             points.add(new Point(x, y));
         }
 
-        return Optional.of(points);
+        try {
+            return Optional.of(points);
+        }catch(Exception e){
+            return Optional.empty();
+        }
     }
 
     public static Optional<List<Point>> fromFile(String path) {
-        throw new UnsupportedOperationException();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+            List<String> lines = new ArrayList<>();
+            do{
+                line = reader.readLine();
+                lines.add(line);
+            } while (line != null);
+            reader.close();
+            return fromString(lines);
+        } catch(Exception e){
+            return Optional.empty();
+        }
     }
 }
